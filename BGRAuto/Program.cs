e484749b638085cd19deba421b1f7b020aa8cf87
WcLyic <saberhana@yandex.com>
2020-08-27 20:40:20 +0800

    try another

--- BGRAuto/Program.cs
+++ BGRAuto/Program.cs
@@ -4,7 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Xml;
 using System.Xml.Linq;
@@ -15,7 +18,7 @@ namespace BGRAuto
 {
     class Program
     {
-        private const string CdnBase = "https://via.hypothes.is/http://ps1.str.cloudn-service.com/android_res/";
+        private const string CdnBase = "http://ps1.str.cloudn-service.com/android_res/";
         private const string Version = CdnBase + "version/version-android.xml";
 
         private static readonly byte[] _aesKey = Convert.FromBase64String("ExmGM04QVp52sgGnwCwoTO+imSnWTXg5y+QrpVBCXNs=");
@@ -52,6 +55,12 @@ namespace BGRAuto
 
         private static void Upload()
         {
+            if (!File.Exists("download.sh"))
+            {
+                var u = Upload("version-android.xml");
+                Box(BoxPath, u);
+            }
+
             if (File.Exists("upload.sh"))
             {
                 var process = Process.Start("bash", "upload.sh");
@@ -148,12 +157,77 @@ namespace BGRAuto
                 Sign.Release();
         }
 
+        private static string Box(string path, string path2 = "")
+        {
+            var handler = new HttpClientHandler();
+#if DEBUG
+            handler.Proxy = new WebProxy("127.0.0.1", 1083);
+#endif
+            using var client = new HttpClient(handler);
+
+            var flag = string.IsNullOrEmpty(path2);
+            var responseMessage =
+                !flag
+                    ? client.PutAsync(path,
+                        new StringContent($"{{\"url\":\"{path2}\"}}", Encoding.UTF8, "application/json")).Result
+                    : client.GetAsync(path).Result;
+
+            // ensure the request was a success
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                return Box(path, path2);
+            }
+
+            if (!flag) return "";
+
+            var result = responseMessage.Content.ReadAsStringAsync().Result;
+            return uRegex.Match(result).Groups[1].Value;
+        }
+
+        private static readonly Regex uRegex = new Regex("\"url\":\"(.*?)\"", RegexOptions.Compiled);
+
+        private const string BoxPath = "https://jsonbox.io/box_e2be6ea4baaa037a5a56/5f47a03558379400178b22be";
+
+        private static string Upload(string path)
+        {
+            var handler = new HttpClientHandler();
+#if DEBUG
+            handler.Proxy = new WebProxy("127.0.0.1", 1083);
+#endif
+            using var client = new HttpClient(handler);
+            using var formData = new MultipartFormDataContent();
+
+            //formData.Headers.Add("albumid", "365");
+            //formData.Headers.Add("token", "6447gtmATU8r9LCPJl1cKOQFQaD1gYIdzFWIR0K3oDjVfL3P6JPezntmcER7faVK");
+            formData.Headers.Add("filelength", "");
+            client.DefaultRequestHeaders.Add("age", (14 * 24).ToString());
+
+            formData.Add(new ByteArrayContent(File.ReadAllBytes(path)), "files[]",
+                "file" + Path.GetExtension(path));
+
+            var response = client.PostAsync("https://safe.fiery.me/api/upload", formData).Result;
+
+            // ensure the request was a success
+            if (!response.IsSuccessStatusCode)
+            {
+                return Upload(path);
+            }
+
+            var result = response.Content.ReadAsStringAsync().Result;
+            return uRegex.Match(result).Groups[1].Value;
+        }
+
         private static void Compare()
         {
             if (File.Exists("download.sh"))
             {
                 var process = Process.Start("bash", "download.sh");
                 process?.WaitForExit();
+            }
+            else
+            {
+                var u = Box(BoxPath);
+                dh.DownloadSingle(u, "version-android2.xml");
             }
 
             dh.DownloadSingle(Version, "version-android.xml");
